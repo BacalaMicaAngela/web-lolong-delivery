85c5ee00ef88c947324a610de0491241139dadc6
BacalaMicaAngela <bacalamica.angela718@gmail.com>
2023-12-03 21:31:42 +0800

    Implement softDelete and restore

--- app/Http/Controllers/RecycleBinController.php
+++ app/Http/Controllers/RecycleBinController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\DeletedUser;
+use Session;
 
 class RecycleBinController extends Controller
 {
@@ -13,16 +14,30 @@ class RecycleBinController extends Controller
         // Fetch deleted users (or any other deleted items you want to display)
         $deletedUsers = User::onlyTrashed()->get();
 
+       
+
         // Return the view with the deleted users
         return view('pages.recycleBin', ['deletedUsers' => $deletedUsers]);
     }
 
     public function index()
     {
-        $deletedUsers = DeletedUser::all();
+        // $deletedUsers = DeletedUser::all();
 
-        // Pass $deletedUsers to the view or perform other actions
-        return view('pages.recycleBin', compact('deletedUsers'));
+        // // Pass $deletedUsers to the view or perform other actions
+        // return view('pages.recycleBin', compact('deletedUsers'));
+
+        $deletedUsers = User::onlyTrashed()->get();
+
+        // Return the view with the deleted users
+        return view('pages.recycleBin', ["data" => $this->user(), 'deletedUsers' => $deletedUsers]);
+    }
+
+    public function user()
+    {
+        if (Session::has('loginId')) {
+            return User::where('user_id', '=', Session::get('loginId'))->first();;
+        }
     }
 }
 
--- resources/views/pages/recycleBin.blade.php
+++ resources/views/pages/recycleBin.blade.php
@@ -1,82 +1,141 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('pages.navigator')
+@section('contentAdmin')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recycle Bin</title>
+<style>
+    .schedule-label {
+        font-size: 20px; /* Adjust the size as needed */
+        font-weight: bold; /* Make the text bold */
+    }
 
-    <!-- Add your CSS styles or include external stylesheets here -->
+    .module-description {
+        font-size: 12px; /* Adjust the size as needed */
+        color: #6B7280; /* Set the color to a muted tone */
+    }
+</style>
 
-    <style>
-        /* Add your custom styles here */
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
+<div class="container-fluid p-3">
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col">
+                    <span class="schedule-label">THRASHED USERS</span>
+                    <p class="module-description">Restore Users</p>
+
+            </div>
+            <div class="card-body">
+                <div style="overflow: auto;">
+                    <table id="dataTable" class="table table-bordered" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Full Name</th>
+                                <th>Deleted At</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($deletedUsers as $user)
+                                <tr>
+                                    <td><p class="mt-3">{{ $user->u_name }}</p></td>
+                                    <td><p class="mt-3">{{ $user->deleted_at }}</p></td>
+                                    <td>
+                                        <center class="mt-3">
+                                        <button data-id="{{ $user->user_id }}" class="btn-sm btn btn-outline-success restoreBtn w-1/2"><i class="fa-solid fa-rotate-left mr-2"></i>Restore</button>
+                                        </center>
+                                    </td>
+                                </tr>
+                             @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+<script>
+
+function Component() {
+                this.msg = (msg, icon) => {
+                    Swal.fire({
+                    position: 'center',
+                    icon: icon,
+                    text: msg,
+                    showConfirmButton: false,
+                    timer: 2000
+                    });
+                }
+
+                this.imgPicker = () => {
+                    camera.onclick = (e) => {
+                        document.querySelector('#fileChooser').click();
+                    }
+
+                    fileChooser.onchange = (e) => {
+                        if (e.target.files && e.target.files[0]) {
+                            document.querySelector('#avatar').src = URL.createObjectURL(event.target.files[0]);
+                        }
+                    }
+                }
+
+                this.imgPicker = () => {
+                    cameraNewUser.onclick = (e) => {
+                        document.querySelector('#fileNewUserChooser').click();
+                    }
+
+                    fileNewUserChooser.onchange = (e) => {
+                        if (e.target.files && e.target.files[0]) {
+                            document.querySelector('#avatarNewUser').src = URL.createObjectURL(event.target.files[0]);
+                        }
+                    }
+                }
+
+                this.checkInput = () => {
+                    return true;
+                }
+            }
+
+$(document).on('click', '.restoreBtn', async function (e) {
+    let id = $(this).data('id');
+    restoreUser(id);});
+
+    const restoreUser = async (id) => {
+        console.log(id);
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'Restore user from Recycle Bin?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Restore'
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            let comp = new Component();
+            let response = await fetch(`restoreUser/${id}`, {
+                method: 'GET',
+                credentials: "same-origin"
+            });
+
+            let { message, status } = await response.json();
+
+            console.log(message, status)
+
+            if (status == 'success') {
+                comp.msg(
+                    message,
+                    status
+                );
+                setTimeout(() => {
+                    location.reload();
+                }, 2000);
+            } else {
+                comp.msg(
+                    message,
+                    'error'
+                );
+            }
         }
+    });
 
-        table, th, td {
-            border: 1px solid black;
-        }
-
-        th, td {
-            padding: 10px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-        /* Add any additional styles as needed */
-    </style>
-</head>
-
-<body>
-
-    <div class="container">
-        <h1>Recycle Bin</h1>
-
-        @if(count($deletedUsers) > 0)
-            <table>
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>Name</th>
-                        <th>Deleted At</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($deletedUsers as $user)
-                        <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->deleted_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @else
-            <p>No items in the Recycle Bin.</p>
-        @endif
-    </div>
-
-    <script>
-        $(document).ready(function() {
-            // Add the following code to set the 'active' class for the Recycle Bin link
-            $('.list-group-item').removeClass('active'); // Remove active class from all links
-            $('.list-group-item[href="/recycleBin"]').addClass('active'); // Add active class to Recycle Bin link
-        });
-
-        // Your JavaScript code for handling restore actions
-        $(document).on('click', '.restoreBtn', function () {
-            var userId = $(this).data('id');
-
-           
-        });
-    </script>
-</body>
-
-</html>
+}
+</script>
+@endsection
+@include('pages.Modals.manageProfile')
